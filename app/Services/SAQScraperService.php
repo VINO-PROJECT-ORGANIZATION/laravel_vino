64f160a27056f0a0d1b9a4ec9266ce2e26e38447
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-04-21 15:15:29 -0400

    Ajouter la méthode chercherVin pour scraper les données des vins depuis SAQ

--- app/Services/SAQScraperService.php
+++ app/Services/SAQScraperService.php
@@ -4,4 +4,22 @@ namespace App\Services;
 
 use Goutte\Client;
 
-class SAQScraperService {}
+class SAQScraperService
+{
+    public function chercherVin($url = 'https://www.saq.com/fr/produits/vin')
+    {
+        $client = new Client();
+        $crawler = $client->request('GET', $url);
+
+        $results = [];
+
+        $crawler->filter('.product-item')->each(function ($node) use (&$results) {
+            $results[] = [
+                'nom' => $node->filter('.product-item-name')->text(),
+                'prix' => $node->filter('.price')->text(),
+                'url' => $node->filter('a.product-item-link')->attr('href'),
+            ];
+        });
+        return $results;
+    }
+}
