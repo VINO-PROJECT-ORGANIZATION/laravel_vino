8d9f54a071184b33a8acbe45527fa33288236bb3
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-04-23 10:12:29 -0400

    Corriger l'attribut 'srcset' en 'src' pour l'image dans la méthode chercherVin

--- app/Services/SAQScraperService.php
+++ app/Services/SAQScraperService.php
@@ -18,7 +18,7 @@ class SAQScraperService
                 'nom' => $node->filter('.product-item-name')->text(),
                 'prix' => $node->filter('.price')->text(),
                 'url' => $node->filter('a.product-item-link')->attr('href'),
-                'img' => $node->filter('img.product-image-photo')->attr('srcset')
+                'img' => $node->filter('img.product-image-photo')->attr('src')
             ];
         });
         return $results;
