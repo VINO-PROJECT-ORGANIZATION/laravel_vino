b2cbb2dcf3ab81bf26235d1b5f4910a74f5bbf9b
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-04-24 11:40:44 -0400

    Enregistrer les bouteilles récupérées dans la base de données et rendre le champ 'bue' nullable dans la migration

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -12,6 +12,22 @@ class ScraperController extends Controller
         $codes_saq = $scraper->chercherSAQCodes();
         $bouteilles = $scraper->chercherBouteilles($codes_saq);
         // Enregistrer les bouteilles dans la base de données
+        foreach ($bouteilles as $bouteille) {
+            \App\Models\Bouteille::updateOrCreate(
+                ['code_saq' => $bouteille['code_saq']],
+                [
+                    'nom' => $bouteille['nom'],
+                    'pays' => $bouteille['pays'],
+                    'code_saq' => $bouteille['code_saq'],
+                    'prix_saq' => $bouteille['prix_saq'],
+                    'url_saq' => $bouteille['url_saq'],
+                    'url_image' => $bouteille['url_image'],
+                    'format' => $bouteille['format'],
+                    'type' => $bouteille['type'],
+                    'bue' => 4, // 4 = non bue
+                ]
+            );
+        }
         return view('scrapper.index', compact('codes_saq', 'bouteilles'));
     }
 }
