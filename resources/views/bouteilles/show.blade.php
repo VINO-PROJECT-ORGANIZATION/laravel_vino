147ad359ad5d64fda8681e6fc6ff71417113ee34
david <therich15@yahoo.fr>
2025-04-28 10:26:37 -0400

    ajout dune paragraphe

--- resources/views/bouteilles/show.blade.php
+++ resources/views/bouteilles/show.blade.php
@@ -1,1 +1,2 @@
 <h1>hell not!!!</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum id error earum consequuntur laudantium minima cum assumenda sunt expedita officia quis explicabo magnam, nam rem, quae ipsa nostrum architecto impedit.</p>
