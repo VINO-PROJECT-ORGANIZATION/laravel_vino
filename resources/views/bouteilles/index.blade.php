804691a7c61a248d4366906e96460ef7d07158d6
david <therich15@yahoo.fr>
2025-04-29 19:48:29 -0400

    les bases html du composant carte

--- resources/views/bouteilles/index.blade.php
+++ resources/views/bouteilles/index.blade.php
@@ -1,3 +1,29 @@
 <x-header-nav-sec></x-header-nav-sec>
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./public/css/styles.css" rel="stylesheet" />
+    <title>Document</title>
+</head>
+<body>
+<div class="carte">
+    <div class="contenant">
+        <img src="" alt="bouteille">
+    </div>
+   <div class="detail">
+    <div class="description"></div>
+    <div class="action"></div>
+   </div>
+</div>
+</body>
+</html>
+
+
+
+
+
 <x-footer></x-footer>
