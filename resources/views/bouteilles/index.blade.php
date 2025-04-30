29b71434ca481d1b35531c021753a1cb8fc4b14d
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-04-30 08:16:40 -0400

    Refactoriser la navigation de l’en-tête et ajouter des fichiers SVG avec logo ; mettre à jour l’image dans la vue index

--- resources/views/bouteilles/index.blade.php
+++ resources/views/bouteilles/index.blade.php
@@ -1,3 +1,4 @@
 <x-header-nav-sec></x-header-nav-sec>
-
+<img src="https://www.saq.com/media/catalog/product/1/5/15460261-1_1744292283.png?optimize=high&fit=bounds&height=166&width=111&canvas=111:166&format=jpeg"
+    alt="">
 <x-footer></x-footer>
