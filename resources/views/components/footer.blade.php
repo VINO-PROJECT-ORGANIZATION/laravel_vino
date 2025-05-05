58d641088748f8607fdfe90d606cd195fae04193
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-05-05 15:54:18 -0400

    Réorganiser le footer pour corriger les liens et mettre à jour les icônes ; remplacer le texte "Bouteilles" par "Recherche" et ajuster les classes actives.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -7,22 +7,23 @@
         <p>Celliers</p>
     </div>
 
+    <div class="@if($pageCourante == '') active @endif">
+        <div class="conteneur">
+            <img src="./images/icons/tirebouchon.svg" alt="icon">
+        </div>
+        <p>Bouteilles</p>
+        </a>
+    </div>
+
     <div class="@if($pageCourante == 'bouteilles') active @endif">
         <!-- lien vers la page des bouteilles -->
         <a href="{{ route('bouteilles.index') }}">
 
             <div class="conteneur">
-                <img src="./images/icons/tirebouchon.svg" alt="icon">
+                <img src="./images/icons/Loupe.svg" alt="icon">
             </div>
-            <p>Bouteilles</p>
+            <p>Recherche</p>
         </a>
-    </div>
-
-    <div class="@if($pageCourante == 'celliers') active @endif">
-        <div class="conteneur">
-            <img src="./images/icons/Loupe.svg" alt="icon">
-        </div>
-        <p>Recherche</p>
     </div>
 
     <div class="@if($pageCourante == 'celliers') active @endif">
