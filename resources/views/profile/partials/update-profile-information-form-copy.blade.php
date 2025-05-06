12974f33954a08cc0f30979e57629b96446b50b4
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-05-06 14:23:18 -0400

    ajout de la gestion de l'adresse et des champs de mot de passe dans le formulaire de mise à jour du profil

--- resources/views/profile/partials/update-profile-information-form-copy.blade.php
+++ resources/views/profile/partials/update-profile-information-form-copy.blade.php
@@ -1,13 +1,13 @@
 <section class="update-profil-form">
     <header>
-        <h2>{{ $user['name'] }}</h2>
+        <h2>{{ $user['prenom']." ".$user['name'] }}</h2>
         <!-- à faire -->
         <p># cellier(s) - 53 Bouteilles</p>
     </header>
     <div class="conteneur">
         <form method="post" action="{{ route('profile.update') }}">
             @csrf
-            @method('update')
+            @method('patch')
             <section>
                 <div class="section__sous-titre">
                     <img src="./images/icons/user.svg" alt="utilisateur">
@@ -30,15 +30,50 @@
             </section>
             <section>
                 <div class="section__sous-titre">
-                    <img src="./images/icons/courriel.svg" alt="email">
+                    <img src="./images/icons/map.svg" alt="email">
                     <h3>Adresse</h3>
                     <img src="./images/icons/back.svg" alt="chevron">
                 </div>
                 <div class="groupe-input">
                     <label for="adresse">Adresse</label>
-                    <textarea name="adresse" id="adresse"></textarea>
+                    <textarea name="adresse" id="adresse" required>{{ old('adresse', $user['adresse']) }}</textarea>
                 </div>
             </section>
+            <input type="email" id="email" name="email" value="{{ $user['email'] }}" hidden>
+
+            <input type="submit" value="Enregistrer" class="bouton">
+        </form>
+    </div>
+    <div class="conteneur">
+        <form method="post" action="{{ route('password.update') }}">
+            @csrf
+            @method('put')
+            <section>
+                <div class="section__sous-titre">
+                    <img src="./images/icons/courriel.svg" alt="courriel">
+                    <h3>Courriel & mot de passe</h3>
+                    <img src="./images/icons/back.svg" alt="chevron">
+                </div>
+                <div class="groupe-input">
+                    <label for="email">Courriel</label>
+                    <input type="email" id="email" name="email" value="{{ $user['email'] }}" readonly>
+                </div>
+                <div class="groupe-input">
+                    <label for="update_password_current_password">Mot de passe courrant</label>
+                    <!-- afficher l'encien mot de passe -->
+                    <input type="password" id="update_password_current_password" name="current_password">
+                </div>
+                <div class="groupe-input">
+                    <label for="update_password_password">Nouveau mot de passe</label>
+                    <input type="password" id="update_password_password" name="password" required>
+                </div>
+                <div class="groupe-input">
+                    <label for="update_password_password_confirmation">Confirmer le mot de passe</label>
+                    <input type="password" id="update_password_password_confirmation" name="password_confirmation"
+                        required>
+                </div>
+            </section>
+            <input type="submit" value="Enregistrer" class="bouton danger">
         </form>
     </div>
 </section>
