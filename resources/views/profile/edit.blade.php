6c1d0082a6aa841b6832aa4061b4d4d041f328cf
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-05-07 09:20:51 -0400

    changement sur les composants;

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,5 +1,5 @@
-<x-header-nav-sec></x-header-nav-sec>
+<x-header-nav-sec />
 <main>
     @include('profile.partials.update-profile-information-form')
 </main>
-<x-footer :pageCourante="$pageCourante"></x-footer>
+<x-footer :pageCourante="$pageCourante" />
