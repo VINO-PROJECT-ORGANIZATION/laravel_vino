7c13e35ef62e8f64e9a9ca0e9a5c9befbc665f3a
david <therich15@yahoo.fr>
2025-05-07 20:02:23 -0400

    structure de base du header de la page accueil

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -21,16 +21,22 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <!-- <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a> -->
-            </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+       <!-- structure de base pour le header de la page d'accueil -->
+        <header class="header__accueil">
+        <nav class="header__nav-sec">
+            <div class="bouton bouton_nav contenant_logo">                
+                    <img src="./images/icons/back.svg" alt="logo">
+
             </div>
-        </div>
+            <div class="bouton bouton_nav contenant_icone" >
+                <img src="" alt="icone">
+            </div>
+        </nav>       
+    </header>
+
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                {{ $slot }}
+    </div>
     </body>
 </html>
