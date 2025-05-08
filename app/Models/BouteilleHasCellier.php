864d2e02d047fae4077b212a59b31aa22b40617b
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-05-08 00:05:57 -0400

    Ajout des méthodes et des propriétés pour gérer les relations et les filtres dans le modèle BouteilleHasCellier

--- app/Models/BouteilleHasCellier.php
+++ app/Models/BouteilleHasCellier.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class BouteilleHasCellier extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'bouteille_id',
+        'cellier_id',
+        'quantite',
+        'favoris',
+    ];
+
+    public function bouteille()
+    {
+        return $this->belongsTo(Bouteille::class);
+    }
+    public function cellier()
+    {
+        return $this->belongsTo(Cellier::class);
+    }
+    public function scopeFavoris($query)
+    {
+        return $query->where('favoris', true);
+    }
+    public function scopeQuantite($query)
+    {
+        return $query->where('quantite', '>', 0);
+    }
 }
