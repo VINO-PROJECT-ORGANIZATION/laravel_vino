54c626aac9ac1c51a90b6ef552ffd0f793b92e1d
david <therich15@yahoo.fr>
2025-05-07 20:09:44 -0400

    ajout de l'icone et du logo dans les balises

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -26,11 +26,11 @@
         <header class="header__accueil">
         <nav class="header__nav-sec">
             <div class="bouton bouton_nav contenant_logo">                
-                    <img src="./images/icons/back.svg" alt="logo">
+                    <img src="./images/logo_vino.svg" alt="logo">
 
             </div>
             <div class="bouton bouton_nav contenant_icone" >
-                <img src="" alt="icone">
+                <img src="./images/icons/hamburger.svg" alt="icone">
             </div>
         </nav>       
     </header>
