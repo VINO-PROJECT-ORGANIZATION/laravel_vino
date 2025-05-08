36e906f28bd5a3695f2d02b983634fd64b6de3f2
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-05-08 11:45:49 -0400

    Ajouter l'affichage des bouteilles dans la vue des bouteilles par cellier

--- resources/views/bouteille_has_cellier/par_cellier.blade.php
+++ resources/views/bouteille_has_cellier/par_cellier.blade.php
@@ -1,5 +1,14 @@
 <x-header-nav-sec />
 <main>
     <h1>bouteilles par cellier</h1>
+    <!-- example de bouteilles par cellier -->
+    @foreach ($bouteilles as $bouteille)
+    <div class="bouteille-cellier">
+        <h2>{{ $bouteille->nom }}</h2>
+        <p>Cellier: {{ $bouteille->cellier->nom }}</p>
+        <p>Quantité: {{ $bouteille->quantite }}</p>
+    </div>
+
+    @endforeach
 </main>
 <x-footer :pageCourante="$pageCourante" />
