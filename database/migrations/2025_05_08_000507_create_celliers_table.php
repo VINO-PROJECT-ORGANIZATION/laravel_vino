a1512866dd9b5729452389f781284c91b3ac8e73
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-05-07 20:08:56 -0400

    Ajout des colonnes 'nom' et 'teinte' à la table 'celliers' dans la migration

--- database/migrations/2025_05_08_000507_create_celliers_table.php
+++ database/migrations/2025_05_08_000507_create_celliers_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('celliers', function (Blueprint $table) {
             $table->id();
+            $table->string('nom')->unique();
+            $table->string('teinte')->nullable();
+            // user_id
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
