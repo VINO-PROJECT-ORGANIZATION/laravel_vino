cf3280c1d7bebc030707f3c1856004817c622de6
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-05-07 21:54:54 -0400

    Ajout de la migration pour créer la table bouteille_has_cellier

---
+++ database/migrations/2025_05_08_015412_create_bouteille_has_cellier_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bouteille_has_cellier', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bouteille_has_cellier');
+    }
+};
