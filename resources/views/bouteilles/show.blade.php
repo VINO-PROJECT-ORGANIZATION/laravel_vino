63dbaaafb8c1d9fa606b65c35aa684e3a0beda82
david <therich15@yahoo.fr>
2025-05-10 13:51:53 -0400

    structure de base pour la composante de message

--- resources/views/bouteilles/show.blade.php
+++ resources/views/bouteilles/show.blade.php
@@ -1,2 +1,15 @@
 <h1>hell not!!!</h1>
 <h1>hell yes!!!</h1>
+<x-header-nav-sec/>
+ <div class="composante_message">
+        <div class="fermer">
+            <p>X</p>
+        </div>
+        <p class="texte-message">
+            Vous avez ajouté 
+            <strong>"19 Crimes Cabernet-Sauvignon"</strong> 
+            a votre cellier
+            <strong>"Cellier de banlieu"</strong> 
+            </p>
+        </p>
+    </div>
