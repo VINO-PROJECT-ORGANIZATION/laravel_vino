f11c690aaf36b35efde8d8283d4c65dc5b414216
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-05-13 23:42:37 -0400

    réorganise la structure des actions de cellier et supprime le code redondant

--- resources/views/celliers/index.blade.php
+++ resources/views/celliers/index.blade.php
@@ -1,6 +1,14 @@
 <x-header-nav-sec></x-header-nav-sec>
 <main>
     <h1>Mes celliers</h1>
+    <div class="celliers-carte__actions">
+        <div class="container">
+            <a href="{{ route('celliers.create') }}" class="bouton">Créer un cellier</a>
+        </div>
+        <div class="container">
+            <a href="{{ route('bouteilles.index') }}" class="bouton">Ajouter des bouteilles</a>
+        </div>
+    </div>
     <div class="container">
         @foreach ($celliers as $cellier)
         <!-- TEST -->
@@ -22,21 +30,10 @@
             <a href="{{ route('cellier_bouteilles.cellier.bouteilles', ['cellier_id' => $cellier->id]) }}">
                 <h2 class="celliers-carte__titre">{{ $cellier['nom'] }}</h2>
             </a>
+            <!-- afficher le nombre de bouteille par cellier -->
             <p class="celliers-carte__description">{{ $quantiteBouteilles[$cellier->id] }} bouteille(s)</p>
-            <!-- edition du cellier -->
-
-            <!-- fin edition du cellier -->
         </section>
         @endforeach
-        <!-- afficher le nombre de bouteille par cellier -->
-
     </div>
-    <div class="container">
-        <a href="{{ route('celliers.create') }}">Créer un cellier</a>
-    </div>
-    <div class="container">
-        <a href="{{ route('bouteilles.index') }}">Ajouter des bouteilles</a>
-    </div>
-
 </main>
 <x-footer :pageCourante="$pageCourante"></x-footer>
