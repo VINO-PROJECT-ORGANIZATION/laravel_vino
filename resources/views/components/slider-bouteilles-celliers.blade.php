e54d0bb1267316bc7d4037f181a6a7ffe588e595
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-05-15 00:03:58 -0400

    Ajout du composant slider-bouteilles-celliers pour afficher les bouteilles

---
+++ resources/views/components/slider-bouteilles-celliers.blade.php
@@ -0,0 +1,10 @@
+<section class="slider__bouteilles-celliers">
+    <h2>Vos bouteilles</h2>
+    <div class="slider__bouteilles-celliers_container">
+        @foreach ($bouteilles as $bouteille)
+        <div class="slider__bouteilles-celliers-container-item">
+
+        </div>
+        @endforeach
+    </div>
+</section>
