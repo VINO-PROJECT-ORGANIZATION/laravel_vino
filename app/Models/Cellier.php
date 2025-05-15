0cc18067b3c846769c03a42f0140d1d8c2f80e7b
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-05-15 00:03:14 -0400

    Ajout de la relation many-to-many entre Cellier et Bouteille avec les attributs quantite et favoris

--- app/Models/Cellier.php
+++ app/Models/Cellier.php
@@ -18,4 +18,10 @@ class Cellier extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function bouteilles()
+    {
+        return $this->belongsToMany(Bouteille::class, 'bouteille_has_celliers', 'cellier_id', 'bouteille_id')
+            ->withPivot('quantite', 'favoris');
+    }
 }
