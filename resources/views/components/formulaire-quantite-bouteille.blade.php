944a0ca9a9e6d79fdcd232d05b825d5cdcd6c956
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-05-15 15:14:54 -0400

    Corriger la structure du formulaire pour mettre à jour la quantité d'une bouteille dans le cellier

--- resources/views/components/formulaire-quantite-bouteille.blade.php
+++ resources/views/components/formulaire-quantite-bouteille.blade.php
@@ -1,10 +1,10 @@
 <!-- Formulaire pour afficher le nombre de bouteilles identique dans le cellier -->
 <form
     action="{{ route('cellier_bouteilles.update.quantite', ['cellier_id' => $reponse->cellier_id, 'bouteille_id' => $reponse->bouteille_id]) }}"
-    method="POST">
+    method="POST" class="formulaire-quantite">
     @csrf
     @method('PUT')
-    <div class="formulaire-quantite">
+    <div class="input-group">
         <label for="quantite">quantité</label>
         <input type="number" id="quantite" name="quantite" min="1" max="100" value="{{ $reponse->quantite }}">
     </div>
