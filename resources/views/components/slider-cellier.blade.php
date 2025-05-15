36bb1493f3b352ab8f24e7472e33498b5a1e64a1
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-05-15 16:42:38 -0400

    Ajouter un lien pour créer un cellier lorsque l'utilisateur n'en a pas

--- resources/views/components/slider-cellier.blade.php
+++ resources/views/components/slider-cellier.blade.php
@@ -8,6 +8,7 @@
         @empty
         <div class="slider__cellier-container-item">
             <p>Vous n'avez pas de cellier !</p>
+            <div class="bouton"><a href="{{ route("celliers.create") }}">Créé un cellier</a></div>
         </div>
         @endforelse
     </div>
