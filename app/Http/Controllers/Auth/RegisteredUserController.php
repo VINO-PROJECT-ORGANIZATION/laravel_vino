befb4768561247f1813017273f03f61b4d9a9429
david <therich15@yahoo.fr>
2025-05-14 23:07:57 -0400

    traduction des messages d'erreurs dans le formulaire d'enregistrement

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,14 +32,27 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255','min:3'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
-            'prenom' => ['required', 'string', 'max:255'],
+            'prenom' => ['required', 'string', 'max:255','min:3'],
             'date_naissance' => ['required', 'date'],
             'adresse' => ['required', 'string', 'max:255'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
             'admin' => ['boolean'],
-        ]);
+        ]
+    ,
+[
+    'name.required' => 'Le nom est obligatoire.',
+    'name.max' => 'Le nom ne doit pas depasser 255 caracteres.',
+    'name.min' => 'Le nom doit avoir plus de 3 caracteres.',
+    'prenom.min' => 'Le prenom doit avoir plus de 3 caracteres.',
+     'prenom.required' => 'le prenom est obligatoire.',
+    'prenom.max' => 'Le prenom ne doit pas depasser 255 caracteres',
+     'adresse.required' => 'L\'addresse est obligatoire',
+    'adresse.max' => 'L\'addresse ne doit pas depasser 255 caracteres',
+    'password.required' => 'Le mot de passe est obligatoire.',
+    'password.confirmed' => 'Le mot de passe requis ne correspond au mot de passe repete.',
+]);
 
         $user = User::create([
             'name' => $request->name,
