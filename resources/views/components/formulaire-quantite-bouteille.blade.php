367b98ce06011b45b81ee2bfa030fceca585356b
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-05-15 14:56:01 -0400

    Ajouter un formulaire pour mettre à jour la quantité d'une bouteille dans le cellier

---
+++ resources/views/components/formulaire-quantite-bouteille.blade.php
@@ -0,0 +1,12 @@
+<!-- Formulaire pour afficher le nombre de bouteilles identique dans le cellier -->
+<form
+    action="{{ route('cellier_bouteilles.update.quantite', ['cellier_id' => $reponse->cellier_id, 'bouteille_id' => $reponse->bouteille_id]) }}"
+    method="POST">
+    @csrf
+    @method('PUT')
+    <div class="formulaire-quantite">
+        <label for="quantite">quantité</label>
+        <input type="number" id="quantite" name="quantite" min="1" max="100" value="{{ $reponse->quantite }}">
+    </div>
+    <button type="submit" class="boutons">Mettre à jour la quantité</button>
+</form>
