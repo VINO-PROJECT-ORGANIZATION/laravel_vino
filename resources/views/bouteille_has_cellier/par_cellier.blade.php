163ba894025a383c91bf73508f2e4a5ae5fa6027
Sammylood <sammy.romeo@hotmail.com>
2025-05-21 11:20:44 -0400

    Ajout de la section de recherche avec titre et composante de recherche dans la vue du cellier

--- resources/views/bouteille_has_cellier/par_cellier.blade.php
+++ resources/views/bouteille_has_cellier/par_cellier.blade.php
@@ -1,11 +1,10 @@
 <x-header-nav-sec />
-<x-composante-recherche :pageCourante="$pageCourante" :pays="$listePays" />
 
 <main class="cellier-page">
     <h1 class="celliers-carte__titre">{{ $cellier->nom }}</h1>
-    
 
-   
+
+
     <section class="cellier-carte">
         <div class="cellier-carte__image @if ($cellier->teinte == '#F28B82') rouge-framboise
                 @elseif ($cellier->teinte == '#FBC4AB') rose-peche
@@ -20,28 +19,32 @@
             @endif">
             <img src="{{asset('images/icons/celliers-icon-01.svg')}}" alt="Image de cellier" />
         </div>
-<section>
-        <!-- liens pour modifier & supprimer un cellier -->
-        <div class="celliers-carte__actions">
+        <section>
+            <!-- liens pour modifier & supprimer un cellier -->
+            <div class="celliers-carte__actions">
 
-            <div class="container"><a href="{{ route('celliers.edit', ['id' => $cellier->id]) }}" class="bouton bouton-warning"> <i class="fa fa-pencil-square-o"></i> <span> Modifier le
-                        cellier</span></a>
+                <div class="container"><a href="{{ route('celliers.edit', ['id' => $cellier->id]) }}" class="bouton bouton-warning"> <i class="fa fa-pencil-square-o"></i> <span> Modifier le
+                            cellier</span></a>
+                </div>
+                <div class="container">
+                    <form action="{{ route('celliers.destroy', ['id' => $cellier->id]) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="bouton bouton-danger"> <i class="fa fa-trash-o"></i><span> Supprimer le cellier</span></button>
+                    </form>
+                </div>
+
             </div>
-            <div class="container">
-                <form action="{{ route('celliers.destroy', ['id' => $cellier->id]) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="bouton bouton-danger"> <i class="fa fa-trash-o"></i><span> Supprimer le cellier</span></button>
-                </form>
-            </div>
-
-        </div>
- </section>
-
+        </section>
+        
+        <section class="section-recherche">
+            <h2 class="section-recherche__titre">Recherche de bouteilles dans le cellier</h2>
+            <x-composante-recherche :pageCourante="$pageCourante" :pays="$listePays" />
+        </section>
 
         <div>
             <a href="{{ route('cellier_bouteilles.cellier.bouteilles', ['cellier_id' => session('id_cellier')]) }}">Tous
-                les resultats</a>
+                les résultats</a>
         </div>
         @if($bouteilles->isEmpty())
         <section>
@@ -50,8 +53,7 @@
                 <ul>Désolé, aucun résultat trouvé.
                     <li>Essayez une autre recherche</li>
                     <br>
-                    <a href="{{ route('cellier_bouteilles.cellier.bouteilles', ['cellier_id' => session('id_cellier')]) }}"
-                        class="bouton">Retour au cellier</a>
+                    
                 </ul>
             </div>
         </section>
