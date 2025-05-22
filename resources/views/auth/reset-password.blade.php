6bdbc26494eaa5446876f6a456f06f75aeba5e08
Sammylood <sammy.romeo@hotmail.com>
2025-05-22 19:22:00 -0400

    Ajustement de style de la page de reset password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -4,42 +4,44 @@
         <header>
             <h1>Création du nouveau mot de passe</h1>
         </header>
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
-            @method('PUT')
+        <div class="enregistrement-form__container balise-form">
+            <form method="POST" action="{{ route('password.update') }}" class="enregistrement-form__container__contenu">
+                @csrf
+                @method('PUT')
 
-            <!-- Jeton -->
-            <input type="hidden" name="token" value="{{ request()->route('token') }}">
+                <!-- Jeton -->
+                <input type="hidden" name="token" value="{{ request()->route('token') }}">
 
-            <!-- Email -->
-            <div class="groupe-input balise_courriel">
-                <label for="email">Adresse courriel</label>
-                <input id="email" type="email" name="email" value="{{ old('email', request('email')) }}" required autofocus>
-                <x-input-error :messages="$errors->get('email')" />
-            </div>
+                <!-- Email -->
+                <div class="groupe-input balise_courriel">
+                    <label for="email">Adresse courriel</label>
+                    <input id="email" type="email" name="email" value="{{ old('email', request('email')) }}" required autofocus>
+                    <x-input-error :messages="$errors->get('email')" />
+                </div>
 
-            <!-- Mot de passe -->
-            <div class="groupe-input balise_password">
-                <label for="password">Créez votre nouveau mot de passe</label>
-                <input type="password" id="password" name="password" required>
-                <x-input-error :messages="$errors->get('password')" />
-            </div>
+                <!-- Mot de passe -->
+                <div class="groupe-input balise_password">
+                    <label for="password">Créez votre nouveau mot de passe</label>
+                    <input type="password" id="password" name="password" required>
+                    <x-input-error :messages="$errors->get('password')" />
+                </div>
 
-            <!-- Confirmation du mot de passe -->
-            <div class="groupe-input balise_password">
-                <label for="password_confirmation">Veuillez confirmer le nouveau mot de passe</label>
-                <input type="password" id="password_confirmation" name="password_confirmation" required>
-                <x-input-error :messages="$errors->get('password_confirmation')" />
-            </div>
+                <!-- Confirmation du mot de passe -->
+                <div class="groupe-input balise_password">
+                    <label for="password_confirmation">Veuillez confirmer le nouveau mot de passe</label>
+                    <input type="password" id="password_confirmation" name="password_confirmation" required>
+                    <x-input-error :messages="$errors->get('password_confirmation')" />
+                </div>
 
-            <div>
-                <x-primary-button class="bouton">{{ __('Enregister') }}</x-primary-button>
-                @if (session('status') === 'password-updated')
-                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
-                    class="">{{ __('Enregirstré.') }}</p>
-                @endif
-            </div>
-        </form>
+                <div class="groupe-input">
+                    <x-primary-button class="boutons btn btn-primary">{{ __('Enregister') }}</x-primary-button>
+                    @if (session('status') === 'password-updated')
+                    <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                        class="">{{ __('Enregirstré.') }}</p>
+                    @endif
+                </div>
+            </form>
+        </div>
     </section>
 </main>
 <x-footer :pageCourante="$pageCourante" />
